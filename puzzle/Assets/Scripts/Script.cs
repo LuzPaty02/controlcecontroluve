dee351b777cc174512b879d4a8e805c6af2bd3ec
Kassandra <a01642447@tec.mx>
2024-04-27 13:46:07 -0600

    botones y menu llistos

--- puzzle/Assets/Scripts/Script.cs
+++ puzzle/Assets/Scripts/Script.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Script : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void play()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Out()
     {
-        
+        Debug.Log("Closing...");
+        Application.Quit();
+
     }
 }
