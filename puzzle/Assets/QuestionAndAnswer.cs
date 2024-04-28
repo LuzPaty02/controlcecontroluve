b3299fdf3374e99371b61242c4eddfbeb62e9a43
LuzPaty02 <luzpatyhr@gmail.com>
2024-04-27 22:03:15 -0600

    errores x

--- puzzle/Assets/QuestionAndAnswer.cs
+++ puzzle/Assets/QuestionAndAnswer.cs
@@ -9,28 +9,3 @@ public class QuestionAndAnswer
 
 }
 
-// Opción #1
-using System;
-using System.Windows.Forms;
-
-namespace EjemploBotonImagen
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-            // Crear un nuevo botón
-            Button botonImagen = new Button();
-            // Establecer la ubicación y el tamaño del botón
-            botonImagen.Location = new System.Drawing.Point(100, 100);
-            botonImagen.Size = new System.Drawing.Size(100, 50);
-            // Establecer una imagen para el botón
-            botonImagen.Image = Image.FromFile("Assets/miImagen.png"); // Ajusta la ruta según la ubicación de tu imagen
-            // Ajustar el tamaño de la imagen al tamaño del botón
-            botonImagen.BackgroundImageLayout = ImageLayout.Stretch;
-            // Añadir el botón al formulario
-            Controls.Add(botonImagen);
-        }
-    }
-}
